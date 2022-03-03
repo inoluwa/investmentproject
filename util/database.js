4d128136a232e31d0c2da31dbe72ca9d7134118b
unknown <naomiadeniran426@gmail.com>
2022-03-03 19:20:59 +0100

    updated

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('excelsheetdata', 'root', '', {
+const sequelize = new Sequelize('heroku_5fd89197d72e0e8', 'b85ec73434fb59', 'ebc094d6', {
           dialect: 'mysql',
-          host: 'localhost',
+          host: 'us-cdbr-east-05.cleardb.net',
 });
 module.exports = sequelize;
